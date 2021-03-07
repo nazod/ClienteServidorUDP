d444c584d678ad11e31521ba847feba5c2c8d34c
Nazod <gabrielricarte80@gmail.com>
2021-03-07 15:17:38 -0300

    comentario na classe program

--- Program.cs
+++ Program.cs
@@ -6,12 +6,15 @@ namespace ServidorClienteUDP
     {
         static void Main(string[] args)
         {
+            //Exemplo de servidor upd que retorna se uma soma é impar ou par, o primeiro a eviar sempre é par.
             UDPSocket s = new UDPSocket();
             s.Server("127.0.0.1", 27000);
 
             UDPSocket c = new UDPSocket();
             c.Client("127.0.0.1", 27000);
+            Console.WriteLine("Escreva um numero: ");
             c.Send(Console.ReadLine());
+            Console.WriteLine("Escreva outro numero: ");
             c.Send(Console.ReadLine());
             c.VencedorParOuImpar();
 

72e84c5d42680fd2ad1cf096255052c2114f40f1
Nazod <gabrielricarte80@gmail.com>
2021-03-07 14:00:59 -0300

    primeiro commit

---
+++ ClienteUDP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClienteUDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Aplicação que envia qualquer valor inserido pelo usuario e retorna esse valor somado com mais 10;
+            //Essa aplicação é para entender os conceitos basicos da comunicação UDP.
+            var client = new UdpClient();
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000); 
+            client.Connect(ep);
+
+            var data = Encoding.ASCII.GetBytes(Console.ReadLine());
+
+            client.Send(data, data.Length);
+
+            var receivedData = client.Receive(ref ep);
+            int.TryParse(Encoding.UTF8.GetString(receivedData), out int inteiroRecbido);
+
+            Console.WriteLine("Dados recebidos de: " + ep.ToString());
+            Console.WriteLine("Valor recido: " + inteiroRecbido.ToString());
+
+            Console.Read();
+        }
+     }
+ }
